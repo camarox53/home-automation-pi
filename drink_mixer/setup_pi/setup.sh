ffc81398be90709021ba0415e688ef3152e3976d
Cameron Morris <camarox53@gmail.com>
2016-02-23 06:38:00 +0000

    Adding to setup script

--- drink_mixer/setup_pi/setup.sh
+++ drink_mixer/setup_pi/setup.sh
@@ -1,8 +1,33 @@
 #! /bin/bash
+# Run with sudos
 
-sudo apt-get udpdate
+# Put SSH Keys into place on root and pi
+wget https://launchpad.net/~cumorris/+sshkeys
+mkdir -p /home/pi/.ssh
+touch /home/pi/.ssh/authorized_keys
+cp +sshkeys /home/pi/.ssh/authorized_keys
+cp +sshkeys /home/root/.ssh/authorized_keys
 
-sudo touch /var/log/drinkmixer.log
-sudo chmod 644 /var/log/drinkmixer.log
-sudo chown www-data:www-data /var/log/drinkmixer.log
+# Update and install new Packages
+apt-get update
+apt-get install apache2 
+
+# Setup log files
+touch /var/log/drinkmixer.log
+chmod 644 /var/log/drinkmixer.log
+chown www-data:www-data /var/log/drinkmixer.log
+
+# Give www-data sudos for scripts
+cp ../files/sudoers /etc/sudoers.d/sudoers
+
+# Copy .htpasswd file into place
+cp ../files/.htpasswd /etc/apache2/.htpasswd
+
+# Copy the apache2 config file into place
+cp ../files/default /etc/apache2/sites-available/default
+
+# Restart apache2 
+service apache2 restart 
+
+
 
