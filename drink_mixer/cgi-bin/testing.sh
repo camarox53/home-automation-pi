6f039cfd5132d54378bb5646af07b075deb4daa4
Cameron Morris <camarox53@gmail.com>
2016-02-23 05:28:50 +0000

    Updating bottle scripts to reflect new path & adding a logfile with correct permissions in setup script

--- drink_mixer/cgi-bin/testing.sh
+++ drink_mixer/cgi-bin/testing.sh
@@ -4,6 +4,6 @@ echo "Content-Type: text/html"
 echo ""
 echo "<html>"
 echo "<p>Your drink is being processed</p>"
-sudo -S python /home/pi/piscripts/bottles/wiskey.py -a oneoz >> /var/log/drinkmixer.log & 
-sudo -S python /home/pi/piscripts/bottles/vodka.py -a oneoz >> /var/log/drinkmixer.log & 
+sudo -S python /home/pi/piscripts/drink_mixer/bottles/wiskey.py -a oneoz >> /var/log/drinkmixer.log & 
+sudo -S python /home/pi/piscripts/drink_mixer/bottles/vodka.py -a oneoz >> /var/log/drinkmixer.log & 
 echo "</html>"
--- drink_mixer/setup_pi/setup.sh
+++ drink_mixer/setup_pi/setup.sh
@@ -1,3 +1,8 @@
 #! /bin/bash
 
 sudo apt-get udpdate
+
+sudo touch /var/log/drinkmixer.log
+sudo chmod 644 /var/log/drinkmixer.log
+sudo chown www-data:www-data /var/log/drinkmixer.log
+
